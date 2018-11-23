9ba0d456504c8f92483bcaf5dc7ededb5d409b95
peren717 <peren717@gmail.com>
2018-11-23 16:12:09 -0700

    fixed a bug that prevents bullets from being fired

--- src/asteroids/game/Controller.java
+++ src/asteroids/game/Controller.java
@@ -265,7 +265,7 @@ public class Controller implements KeyListener, ActionListener
         {
             ship.decelerate();
         }
-        if (e.getKeyCode() == KeyEvent.VK_SPACE && ship != null)
+        if (e.getKeyCode() == KeyEvent.VK_SPACE)
         {
             ship.fire();
         }
