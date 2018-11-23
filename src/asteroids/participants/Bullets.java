f9516aa3304d74e3082d9151c38fa201468e4773
HellianLunaris <45247035+HellianLunaris@users.noreply.github.com>
2018-11-23 14:44:44 -0500

    Bullets Updated
    Added functioning constructor

--- src/asteroids/participants/Bullets.java
+++ src/asteroids/participants/Bullets.java
@@ -1,30 +1,48 @@
+
 package asteroids.participants;
 
-import java.awt.Shape;
 import asteroids.destroyers.AsteroidDestroyer;
-import asteroids.game.Controller;
 import asteroids.game.Participant;
+import asteroids.game.ParticipantCountdownTimer;
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
 
 public class Bullets extends Participant implements AsteroidDestroyer
 {
-
-    public Bullets (double x, double y, double direction, Controller controller)
+    
+    private Shape outline;
+    
+    /*
+    /  Creates new bullet at x and y moving in direction that disappears at the end of the timer.
+    */
+    public Bullets(double x, double y, double direction) 
     {
-        // TODO Auto-generated constructor stub
+        this.setPosition(x, y);
+        this.setVelocity(14.0, direction);
+        this.outline = new Ellipse2D.Double(0.0, 0.0, 1.0, 1.0);
+        new ParticipantCountdownTimer(this, this, 1000);
     }
 
     @Override
-    protected Shape getOutline ()
+    protected Shape getOutline() 
     {
-        // TODO Auto-generated method stub
-        return null;
+        return this.outline;
+    }
+
+    /*
+    / At the end of the timer, remove the bullet.
+    */
+    @Override
+    public void countdownComplete(Object bullet) 
+    {
+        Participant.expire(this);
     }
 
     @Override
     public void collidedWith (Participant p)
     {
-        // TODO Auto-generated method stub
-
+        Participant.expire(this);
+        
     }
-
 }
+
