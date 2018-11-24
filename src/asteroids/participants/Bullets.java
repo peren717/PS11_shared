abb045b1cb3a9abd52fb5c6f301a37baa903e11f
peren717 <peren717@gmail.com>
2018-11-23 17:19:35 -0700

    Update Bullets.java
    auto formated

--- src/asteroids/participants/Bullets.java
+++ src/asteroids/participants/Bullets.java
@@ -9,13 +9,13 @@ import java.awt.geom.Ellipse2D;
 
 public class Bullets extends Participant implements AsteroidDestroyer
 {
-    
+
     private Shape outline;
-    
+
     /*
-    /  Creates new bullet at x and y moving in direction that disappears at the end of the timer.
-    */
-    public Bullets(double x, double y, double direction) 
+     * Creates new bullet at x and y moving in direction that disappears at the end of the timer.
+     */
+    public Bullets (double x, double y, double direction)
     {
         this.setPosition(x, y);
         this.setVelocity(14.0, direction);
@@ -24,16 +24,16 @@ public class Bullets extends Participant implements AsteroidDestroyer
     }
 
     @Override
-    protected Shape getOutline() 
+    protected Shape getOutline ()
     {
         return this.outline;
     }
 
     /*
-    / At the end of the timer, remove the bullet.
-    */
+     * At the end of the timer, remove the bullet.
+     */
     @Override
-    public void countdownComplete(Object bullet) 
+    public void countdownComplete (Object bullet)
     {
         Participant.expire(this);
     }
@@ -44,7 +44,6 @@ public class Bullets extends Participant implements AsteroidDestroyer
         if (p instanceof Asteroid)
         {
             Participant.expire(this);
-        }        
+        }
     }
 }
-
