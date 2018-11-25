df67f8c7a38f97872c85353a423cf1c21381a6d0
HellianLunaris <45247035+HellianLunaris@users.noreply.github.com>
2018-11-24 23:37:10 -0500

    Added asteroid debris participant. Standby for update to add them.

---
+++ src/asteroids/participants/AsteroidDebris.java
@@ -0,0 +1,49 @@
+package asteroids.participants;
+
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
+import asteroids.game.Participant;
+import asteroids.game.ParticipantCountdownTimer;
+
+public class AsteroidDebris extends Participant
+{
+    private Shape outline;
+
+    /*
+     * Creates new piece of debris at x and y moving in direction that disappears at the end of the timer.
+     */
+    public AsteroidDebris (double x, double y, double direction, int num)
+    {
+        while (num > 0)
+        {
+            this.setPosition(x, y);
+            this.setVelocity(1.0, direction);
+            this.outline = new Ellipse2D.Double(0.0, 0.0, 1.0, 1.0);
+            new ParticipantCountdownTimer(this, this, 1000);
+            num--;
+        }
+    }
+    
+
+    @Override
+    protected Shape getOutline ()
+    {
+        return outline;
+    }
+    
+    /*
+     * At the end of the timer, remove the debris.
+     */
+    @Override
+    public void countdownComplete (Object debris)
+    {
+        Participant.expire(this);
+    }
+
+    @Override
+    public void collidedWith (Participant p)
+    {
+        return;
+    }
+
+}
